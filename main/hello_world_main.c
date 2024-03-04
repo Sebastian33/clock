968d5fbc181b1f5dd0925c92c15a26cec80c3f0c
Sebastian33 <seba_mag@ukr.net>
2024-03-04 08:31:53 -0500

    rtc tested

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -23,33 +23,32 @@ void app_main(void)
 
 	u8 buf[7];
     printf("Hello world!\n");
-    i2c_config_t conf;
+    i2c_config_t conf={};
     conf.mode = I2C_MODE_MASTER;
     conf.sda_io_num = 13;
     conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
     conf.scl_io_num = 14;
     conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = 400000;
     i2c_param_config(I2C_NUM_0, &conf);
     i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
 
-    buf[0]=dec2bcd(1);
-    buf[1]=dec2bcd(20);
-    buf[2]=dec2bcd(14);
-    buf[3]=dec2bcd(29);
-    buf[4]=dec2bcd(3);
-    buf[5]=dec2bcd(2) | (1<<7);
+    buf[0]=dec2bcd(0);
+    buf[1]=dec2bcd(10);
+    buf[2]=dec2bcd(20);
+    buf[3]=dec2bcd(2);
+    buf[4]=dec2bcd(5); //weekday
+    buf[5]=dec2bcd(3) | (1<<7); //month
     buf[6]=dec2bcd(24);
 
-    i2c_cmd_handle_t handle = i2c_cmd_link_create();
-    i2c_master_start(handle);
-    i2c_master_write_byte(handle, 0xa2, true);
-    i2c_master_write_byte(handle, 0x02, true);
-    i2c_master_start(handle);
-    i2c_master_write_byte(handle, 0xa2, true);
-    i2c_master_write(handle, buf, 7, true);
-    i2c_master_stop(handle);
-    i2c_master_cmd_begin(I2C_NUM_0, handle, 1000/portTICK_PERIOD_MS);
-    i2c_cmd_link_delete(handle);
+//    i2c_cmd_handle_t handle = i2c_cmd_link_create();
+//    i2c_master_start(handle);
+//    i2c_master_write_byte(handle, 0xa2, true);
+//    i2c_master_write_byte(handle, 0x02, true);
+//    i2c_master_write(handle, buf, 7, true);
+//    i2c_master_stop(handle);
+//    i2c_master_cmd_begin(I2C_NUM_0, handle, 1000/portTICK_PERIOD_MS);
+//    i2c_cmd_link_delete(handle);
 
     for (int i = 10; i >= 0; i--)
     {
@@ -64,7 +63,7 @@ void app_main(void)
    	    i2c_master_cmd_begin(I2C_NUM_0, handle, 1000/portTICK_PERIOD_MS);
    	    i2c_cmd_link_delete(handle);
 
-    	printf("%d:%d:%d %d-%d-%d", bcd2dec(buf[0]&0x7f), bcd2dec(buf[1]&0x7f), bcd2dec(buf[2]&0x3f), bcd2dec(buf[3]&0x3f), bcd2dec(buf[5]&0x1f), bcd2dec(buf[6]));
+    	printf("%d:%d:%d %d-%d-%d\n", bcd2dec(buf[0]&0x7f), bcd2dec(buf[1]&0x7f), bcd2dec(buf[2]&0x3f), bcd2dec(buf[3]&0x3f), bcd2dec(buf[5]&0x1f), bcd2dec(buf[6]));
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
     printf("Restarting now.\n");

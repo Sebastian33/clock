8f928f20c8ae0a9a91b76c1f4e27334e690b5cd1
Sebastian33 <seba_mag@ukr.net>
2024-04-21 19:07:16 -0400

    ntp, date to seconds

--- main/ntp.py
+++ main/ntp.py
@@ -1,4 +1,6 @@
 CENTURY = 3155673600
+DAYS_IN_MONTH = [31,28,31,30,31,30,31,31,30,31,30,31]
+LEAP_BEFORE_1900 = 460
 
 def sec2date(s):
     if s>=CENTURY:
@@ -34,8 +36,19 @@ def sec2date(s):
         
     days=days-leap+1
     years+=years0
-    #print(years, days, hours, minutes, seconds)
-    return (years, days, hours, minutes, seconds)
+
+    month=0
+    while days>DAYS_IN_MONTH[month]:
+        days-=DAYS_IN_MONTH[month]
+        if month==1 and ((years%400==0) or (years%4==0 and years%100!=0)):
+            if days>1:
+                days-=1
+            else:
+                days=DAYS_IN_MONTH[0]
+                break
+        month+=1
+    
+    return (years, month, days, hours, minutes, seconds)
 
 def sec2date2(s):
     years=1900
@@ -60,4 +73,26 @@ def sec2date2(s):
     s+=1
     #print(years, s, hours, minutes, seconds)
     return (years, s, hours, minutes, seconds)
+
+def date2sec(years, months, days, hours, minutes, seconds):
+    seconds+=60*minutes+hours*3600
+
+    for m in range(months-1):
+        days+=DAYS_IN_MONTH[m]
+
+    leap=years//4 
+    leap-=years//100
+    leap+=years//400
+    leap-=LEAP_BEFORE_1900
+
+    if (years%400==0) or (years%4==0 and years%100!=0):
+        leap-=1
+        if months>2:
+            days+=1
+
+    years-=1900
+    days+=years*365+leap-1
+    seconds+=days*3600*24
+    return seconds
+    
 
